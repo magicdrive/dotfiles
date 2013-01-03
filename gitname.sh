896320c53714272bda07e4b11a1385cb31a495aa
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-01-04 03:15:17 +0900

    git name

--- gitname.sh
+++ gitname.sh
@@ -1,2 +1,12 @@
-git config --global user.email hiroshi.ikegami@magicdrive.jp
-git config --global user.user "Hiroshi IKEGAMI"
+#! /bin/bash
+
+echo 'Please tell me, user.name:'
+read name
+git config --global user.name ${name}
+
+echo 'Please tell me, user.email:'
+read email
+git config --global user.email ${email}
+
+
+exit 0;
