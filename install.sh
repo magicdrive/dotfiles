d1f36b7f9a8dada31e3969810533a8848418c020
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-01-10 12:22:02 +0900

    tmux.conf

--- install.sh
+++ install.sh
@@ -10,23 +10,24 @@ cur_dir=$(pwd)
 ## dotfiles list
 
 zsh_files=( '.zsh' '.zshrc' '.zshenv' );
-tmux_files=( '.tmux.conf' '.tmux.env' );
+tmux_files=( '.tmux.conf' '.tmux.env' '.tmux-powerline.conf' );
 screen_files=( '.screenrc' );
 bash_files=( '.bashrc' );
 
+backup_dir=$HOME/.dotfiles.backup
+[ -d ${backup_dir} ] || mkdir -p ${backup_dir};
 
 function install_files () {
 
-    local dir=$1;
-    shift;
+local dir=$1;
+shift;
 
-    for x in `echo "$*"`;
-    do
-        if [ -e ${HOME}/${x} ];then
-           cp -af  ${HOME}/${x} ${HOME}/${x}.${execdatetime}.backup;
-        fi
-        ln -sf ${cur_dir}/rc/${dir}/${x} $HOME/${x}
-    done;
+for x in `echo "$*"`; do
+    if [ -e ${HOME}/${x} ];then
+        cp -af  ${HOME}/${x} ${backup_dir}/${x}.${execdatetime}.backup;
+    fi
+    ln -sf ${cur_dir}/rc/${dir}/${x} $HOME/${x}
+done;
 }
 
 install_files zsh ${zsh_files[@]};
