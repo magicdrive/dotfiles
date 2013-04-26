f0f8cbeeb38d225cbb040ecdd9cb83477b2c57b5
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-04-26 11:15:33 +0900

    vrapper

--- install.sh
+++ install.sh
@@ -13,27 +13,28 @@ zsh_files=( '.zsh' '.zshrc' '.zshenv' );
 tmux_files=( '.tmux.conf' '.tmux.env' '.tmux-powerline.conf' );
 screen_files=( '.screenrc' );
 bash_files=( '.bashrc' );
+vrapper_files=( '.vrapperrc' );
 
 backup_dir=$HOME/.dotfiles.backup
 [ -d ${backup_dir} ] || mkdir -p ${backup_dir};
 
-function install_files () {
+install_files () {
+    local dir=$1;
+    shift;
 
-local dir=$1;
-shift;
-
-for x in `echo "$*"`; do
-    if [ -e ${HOME}/${x} ];then
-        cp -af  ${HOME}/${x} ${backup_dir}/${x}.${execdatetime}.backup;
-    fi
-    ln -sf ${cur_dir}/rc/${dir}/${x} $HOME/${x}
-done;
+    for x in `echo "$*"`; do
+        if [ -e ${HOME}/${x} ];then
+            cp -af  ${HOME}/${x} ${backup_dir}/${x}.${execdatetime}.backup;
+        fi
+        ln -sf ${cur_dir}/rc/${dir}/${x} $HOME/${x}
+    done;
 }
 
 install_files zsh ${zsh_files[@]};
 install_files tmux ${tmux_files[@]};
 install_files screen ${screen_files[@]};
 install_files bash ${bash_files[@]};
+install_files vrapper ${vrapper_files[@]};
 
 exit 0;
 
