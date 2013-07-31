9684ebb9f4d28393b089f84650743d42ac307255
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-07-31 13:31:43 +0900

    update: gen_localdir

--- setup.d/gen_localdir.sh
+++ setup.d/gen_localdir.sh
@@ -2,6 +2,7 @@
 
 cd ${HOME};
 
-mkdir -p bin git etc tmp misc dev/{c,cpp,gosh,js,clojure,scala,ruby,perl,python,shell} share/{man1,man2,man3,man4,man5,man6,man7}
+mkdir -p bin git etc tmp misc dev/{c,cpp,gosh,js,clojure,scala,ruby,perl,python,shell} 
+mkdir -p share/man/{man1,man2,man3,man4,man5,man6,man7}
 
 exit 0;
