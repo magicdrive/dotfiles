97f5e084f36bfbaab5933d35dd9c522ab65802da
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-09-03 13:48:15 +0900

    cert/setup update

--- setup.d/cert/setup.sh
+++ setup.d/cert/setup.sh
@@ -4,13 +4,14 @@
 # setup cert.pem
 #
 
-[ $(uname) != 'Darwin' ] && exit 1
-
-if [ ! $(which brew) ];then
-    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+if [ $(uname -s) = 'Darwin' ];then
+    if [ ! $(which brew) ];then
+        ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+    fi
+    brew install curl-ca-bundle
 fi
 
-brew install curl-ca-bundle
-curl http://curl.haxx.se/ca/cacert.pem -o /usr/local/etc/openssl/cert.pem 
+certfile=/usr/local/etc/openssl/cert.pem
+[ -e ${certfile} ] || curl http://curl.haxx.se/ca/cacert.pem -o ${certfile}
 
 exit 0
