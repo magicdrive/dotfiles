c9706e06595c1da53a35a5ed55f9ff4f606c5e39
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-10-26 22:20:23 +0900

    update

--- setup.d/gen_localdir.sh
+++ setup.d/gen_localdir.sh
@@ -1,13 +1,9 @@
-#! /bin/sh
+#! /bin/bash
 
 cd ${HOME};
 
-
-mkdir -p bin git etc tmp misc
-
-for x in "c cpp gosh js scala ruby perl python shell";do
-    mkdir -p dev/${s}
-done
+mkdir -p bin git etc tmp misc local
+mkdir -p dev/git dev/sand-box
 
 for x in $(seq 9);do
     mkdir -p share/man/man${x}
