f28db8384c203d51bcde00a91d4481bf514ac6ec
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2013-12-01 00:40:17 +0900

    rbenv.sh new plugin: rbenv-binstubs

--- setup.d/ruby/rbenv.sh
+++ setup.d/ruby/rbenv.sh
@@ -13,6 +13,7 @@ git clone git://github.com/sstephenson/ruby-build.git ${installdir}/plugins/ruby
 git clone git://github.com/jamis/rbenv-gemset.git ${installdir}/plugins/rbenv-gemset
 git clone git://github.com/ryansouza/rbenv-rehash.git ${installdir}/plugins/rbenv-rehash
 git clone git://github.com/sstephenson/rbenv-gem-rehash.git ${installdir}/plugins/rbenv-gem-rehash
-
+git clone https://github.com/ianheggie/rbenv-binstubs.git ${installdir}/plugins/rbenv-binstubs
 
 exec $SHELL -l
+
