ffd02d36d7fb1359f1de4ad2f1cfb1e96d8ceb5a
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-03-11 11:35:02 +0900

    update

--- setup.d/python/ipython.sh
+++ setup.d/python/ipython.sh
@@ -8,9 +8,18 @@ fi
 #pip2 install matplotlib
 pip3 install matplotlib
 pip3 install ipython
+pip3 install numpy
+pip3 install scipy
+pip3 install pillow
 
 pip2 install matplotlib
 pip2 install ipython
+pip2 install numpy
+pip2 install scipy
+pip2 install pillow
+
+
+
 pyenv rehash
 
 exit 0
