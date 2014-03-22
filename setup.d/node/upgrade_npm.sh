547a317e72c8a5de332f46f00e2d5e067729ec43
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-03-22 16:54:10 +0900

    udpate

---
+++ setup.d/node/upgrade_npm.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+npm -g uninstall \
+    hotnode \
+    bower \
+    jshint \
+    js-beautify \
+    grunt \
+    mocha \
+    forever \
+    coffee-script \
+    coffeelint \
+    sweet.js \
+    typescript \
+    requirejs \
+    tsi \
+    tsd \
+    tslint \
+;
+./npm.sh
+
