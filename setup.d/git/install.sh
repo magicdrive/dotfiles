5441975573110308ba686f29c5107cfd72e56846
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-04-25 19:56:48 +0900

    git

--- setup.d/git/install.sh
+++ setup.d/git/install.sh
@@ -5,7 +5,7 @@ set -e
 current=$(cd $( dirname $0 ) && pwd)
 
 ${current}/conf.sh
-cp -af ${current}/git-vim-plugin-update $HOME/local/bin
+cp -af ${current}/git-gems $HOME/local/bin
 cp -af ${current}/git-author-def $HOME/local/bin
 
 git author-def -g
