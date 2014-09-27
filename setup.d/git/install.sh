48855c41aa002800dbf512e03e285860fdab91a5
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-09-28 02:56:12 +0900

    update

--- setup.d/git/install.sh
+++ setup.d/git/install.sh
@@ -5,7 +5,6 @@ set -e
 current=$(cd $( dirname $0 ) && pwd)
 
 ${current}/conf.sh
-cp -af ${current}/git-gems $HOME/local/bin
 cp -af ${current}/git-author-def $HOME/local/bin
 cp -af ${current}/git-gen-keepfile $HOME/local/bin
 
