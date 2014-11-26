69be5f57e164c867659215f13939ee743657e8de
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2014-11-27 02:32:53 +0900

    go-package-install.sh

--- setup.d/go/go-package-install.sh
+++ setup.d/go/go-package-install.sh
@@ -4,7 +4,6 @@ set -e
 
 go get -u code.google.com/p/go.tools/cmd/goimports
 go get -u github.com/nsf/gocode
-go get -u github.com/jingweno/gotask
 go get -u github.com/motemen/ghq
 go get -u github.com/tools/godep
 go get -u bitbucket.org/ymotongpoo/goenv
@@ -12,4 +11,14 @@ go get -u bitbucket.org/ymotongpoo/goenv
 # goenv
 cp ${GOPATH}/src/bitbucket.org/ymotongpoo/goenv/shellscripts/goenvwrapper.sh ${GOPATH}/bin/
 
-exit 0;
+cat << 'STRING'
+Packeges install complete.
+Run the following command to activate the goof/goenv is!
+
+    source $(which goenvwrapper.sh)
+
+enjoy!
+
+STRING
+
+exit 0
