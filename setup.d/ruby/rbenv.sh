9ca909e37ee03bcc9895a4c8dd7a65d89d183880
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2018-06-01 10:16:44 +0900

    goodbye gemset

--- setup.d/ruby/rbenv.sh
+++ setup.d/ruby/rbenv.sh
@@ -12,7 +12,6 @@ ln -sfn ${installdir} ~/.rbenv
 ### plugins
 mkdir -p ${installdir}/plugins
 git clone git://github.com/sstephenson/ruby-build.git ${installdir}/plugins/ruby-build
-git clone git://github.com/jamis/rbenv-gemset.git ${installdir}/plugins/rbenv-gemset
 git clone git://github.com/ryansouza/rbenv-rehash.git ${installdir}/plugins/rbenv-rehash
 git clone git://github.com/sstephenson/rbenv-gem-rehash.git ${installdir}/plugins/rbenv-gem-rehash
 git clone https://github.com/ianheggie/rbenv-binstubs.git ${installdir}/plugins/rbenv-binstubs
