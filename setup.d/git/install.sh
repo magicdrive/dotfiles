6ce007bdd934dc987e41ba3cb96e304695658aee
Hiroshi IKEGAMI <hiroshi.ikegami@satori.team>
2020-02-06 06:56:05 +0000

    keepfile

--- setup.d/git/install.sh
+++ setup.d/git/install.sh
@@ -6,7 +6,7 @@ current=$(cd $( dirname $0 ) && pwd)
 
 ${current}/git-conf.sh
 cp -af ${current}/git-author-def $HOME/local/bin
-cp -af ${current}/git-gen-keepfile $HOME/local/bin
+cp -af ${current}/git-keepfile $HOME/local/bin
 cp -af ${current}/git-ffa $HOME/local/bin
 cp -af ${current}/git-refs $HOME/local/bin
 
