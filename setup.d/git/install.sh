28dbed779e77d026cdc12ba8fb9d291fee5137c0
Hiroshi IKEGAMI <hiroshi.ikegami@satori.team>
2020-02-06 06:53:18 +0000

    installer

--- setup.d/git/install.sh
+++ setup.d/git/install.sh
@@ -8,6 +8,7 @@ ${current}/git-conf.sh
 cp -af ${current}/git-author-def $HOME/local/bin
 cp -af ${current}/git-gen-keepfile $HOME/local/bin
 cp -af ${current}/git-ffa $HOME/local/bin
+cp -af ${current}/git-refs $HOME/local/bin
 
 git author-def -g
 
