53a9bf52e45068b56b158ee0014f35c4a096430d
Hiroshi IKEGAMI <hiroshi.ikegami@magicdirve.jp>
2020-02-09 22:15:48 +0900

    git

--- setup.d/git/install.sh
+++ setup.d/git/install.sh
@@ -7,12 +7,7 @@ current=$(cd $( dirname $0 ) && pwd)
 git config --global core.quotepath false
 
 ${current}/git-conf.sh
-cp -af ${current}/bits/git-author-def $HOME/local/bin
-cp -af ${current}/bits/git-keepfile $HOME/local/bin
-cp -af ${current}/bits/git-logf $HOME/local/bin
-cp -af ${current}/bits/git-addf $HOME/local/bin
-cp -af ${current}/bits/git-ignore $HOME/local/bin
-cp -af ${current}/bits/git-refs $HOME/local/bin
+cp -af ${current}/bits/git-* $HOME/local/bin
 
 
 if [ "$1" = "--setup" ];then
