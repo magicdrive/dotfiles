c8e2603967a438134b75d40970f7b5fa700dae77
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2020-02-11 21:49:26 +0000

    dep

---
+++ setup.d/python/mac_dep.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+brew install openssl readline sqlite3 xz zlib
---
+++ setup.d/python/ubuntu_dep.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+sudo apt-get install --no-install-recommends make build-essential zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
