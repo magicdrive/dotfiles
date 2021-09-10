188594093a9270bb2a5df3e2e33e20a8d537f418
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-09-10 06:21:22 +0000

    go

--- setup.d/go/go-package-install.sh
+++ setup.d/go/go-package-install.sh
@@ -6,12 +6,8 @@ mkdir -p ~/projects/gocode/src
 mkdir -p ~/.gopath ~/.gopath/src ~/.gopath/bin ~/.gopath/pkg
 
 go get -u github.com/nsf/gocode
-go get -u github.com/tools/godep
-go get -u github.com/direnv/direnv
 go get -u github.com/pilu/fresh
 go get -u github.com/mattn/goreman
-go get -u github.com/peco/peco/cmd/peco
-go get -u github.com/cespare/reflex
 
 #echo gox -build-toolchain
 #sudo GOROOT="$GOROOT" GOPATH="$GOPATH" PATH="$PATH" "$(which gox)" -build-toolchain
