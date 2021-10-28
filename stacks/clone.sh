3cccd336965ea130eceef6f6ddfa7edec98a5ede
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-10-28 09:27:57 +0000

    Update stacks-install.sh

--- stacks/clone.sh
+++ stacks/clone.sh
@@ -4,11 +4,15 @@ stacks=(vimfiles git-parallel git-gh-diff-url t git-repl gemini docker-repl grea
 
 mkdir -p "${HOME}/git"
 
+print::exec() {
+    echo "$@" && $@
+}
+
 for x in ${stacks[@]}; do
     if [[ ! -d "${HOME}/git/${x}" ]];then
         echo cloning "${x}" ...
         echo
-        git clone "git@github.com:magicdrive/${x}" "${HOME}/git/${x}"
+        print::exec git clone "git@github.com:magicdrive/${x}" "${HOME}/git/${x}"
         echo
         echo
         echo
---
+++ stacks/stacks-install.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+stacks=(git-parallel git-gh-diff-url t git-repl gemini docker-repl)
+
+mkdir -p "${HOME}/git"
+
+print::exec() {
+    echo "$@" && $@
+}
+
+for x in ${stacks[@]}; do
+    if [[ -d "${HOME}/git/${x}" ]];then
+        echo installing "${x}" ...
+        echo
+        print::exec pushd "${HOME}/git/${x}"
+        print::exec make install
+        print::exec popd
+        echo
+        echo
+        echo
+    fi
+done
+
+exit 0
