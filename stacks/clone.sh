e13c32fb4695bc818396890efc62dec68d160329
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2021-10-28 09:19:56 +0000

    stacks/clone.sh

---
+++ stacks/clone.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+stacks=(vimfiles git-parallel git-gh-diff-url t git-repl gemini docker-repl great-thing tmux-powerline)
+
+mkdir -p "${HOME}/git"
+
+for x in ${stacks[@]}; do
+    if [[ ! -d "${HOME}/git/${x}" ]];then
+        echo cloning "${x}" ...
+        echo
+        git clone "git@github.com:magicdrive/${x}" "${HOME}/git/${x}"
+        echo
+        echo
+        echo
+    fi
+done
+
+exit 0
