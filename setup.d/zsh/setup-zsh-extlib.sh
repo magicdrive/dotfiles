99f35cff4bf151d58368421f5388664348fb523e
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2024-07-18 10:40:33 +0900

    update z repo urL

--- setup.d/zsh/setup-zsh-extlib.sh
+++ setup.d/zsh/setup-zsh-extlib.sh
@@ -7,9 +7,10 @@
 libs=('zsh-completions' 'zsh-syntax-highlighting' 'zsh-autosuggestions');
 
 for x in ${libs[@]}; do git clone https://github.com/zsh-users/$x $HOME/git/$x; done;
+    https://github.com/rupa/z.git
 
 # z
-git clone git@github.com:rupa/z.git $HOME/git/z
+git clone https://github.com/rupa/z.git $HOME/git/z
 mkdir -p $HOME/local/share/man/man1/
 sudo ln -sf $HOME/git/z/z.1 $HOME/local/share/man/man1/
 
