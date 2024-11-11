a31ff6de0999e5e9ac4f2b8c9c76460066fce828
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2024-11-11 10:14:07 +0900

    go

--- setup.d/go/install-go-tools.sh
+++ setup.d/go/install-go-tools.sh
@@ -2,9 +2,21 @@
 
 set -e
 
+mkdir -p ~/projects/gocode/src
+mkdir -p ~/.gopath ~/.gopath/src ~/.gopath/bin ~/.gopath/pkg
+
 export GO111MODULE=on
+
 go clean --modcache
+
 go install "github.com/magicdrive/kirke@latest"
+go install "golang.org/x/tools/gopls@latest"
+go install "github.com/nsf/gocode@latest"
+go install "github.com/pilu/fresh@latest"
+go install "github.com/mattn/goreman@latest"
+
+
+echo complete!
 
 
 exit 0
--- setup.d/go/lsp.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-GO111MODULE=on go install golang.org/x/tools/gopls@latest
-
-
-exit 0
