b65343199dec4d513fd1ce8f6ab041e286e5daae
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2025-01-24 10:19:13 +0900

    update go tools

--- setup.d/go/install-go-tools.sh
+++ setup.d/go/install-go-tools.sh
@@ -9,14 +9,41 @@ export GO111MODULE=on
 
 go clean --modcache
 
-go install "github.com/magicdrive/kirke@latest"
-go install "golang.org/x/tools/gopls@latest"
-go install "github.com/nsf/gocode@latest"
-go install "github.com/pilu/fresh@latest"
-go install "github.com/mattn/goreman@latest"
+TOOLS=(
+  "github.com/mdempsky/gocode"
+  "golang.org/x/tools/gopls"
+  "golang.org/x/tools/cmd/goimports"
+  "golang.org/x/tools/cmd/guru"
+  "golang.org/x/tools/cmd/gorename"
+  "github.com/rogpeppe/godef"
+  "golang.org/x/lint/golint"
+  "github.com/kisielk/errcheck"
+  "github.com/jstemmer/gotags"
+  "github.com/fatih/gomodifytags"
+  "github.com/josharian/impl"
+  "github.com/haya14busa/goplay/cmd/goplay"
+  "github.com/go-delve/delve/cmd/dlv"
+  "github.com/mgechev/revive"
+  "honnef.co/go/tools/cmd/staticcheck"
+  "mvdan.cc/gofumpt"
+  "github.com/incu6us/goimports-reviser/v3"
+  "github.com/koron/iferr"
+  "github.com/magicdrive/kirke"
+  "github.com/pilu/fresh"
+  "github.com/mattn/goreman"
+)
+
+echo "Install golang tools..."
+
+for tool in "${TOOLS[@]}"; do
+  echo "Installing: $tool"
+  go install "$tool@latest"
+  echo
+  echo
+done
 
 
-echo complete!
+echo Install complete!
 
 
 exit 0
